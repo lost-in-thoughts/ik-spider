acf829ef63e6b6c689fe9dcb7ca7c9dfbb82f39d
Philipp Bönsch <philipp.boensch@softgames.de>
2020-10-25 18:53:29 +0100

    basic ik implemented for single leg

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 require 'vec2'
 
-local function createArm(root, bone1Length, bone2Length)
+local function createLeg(root, bone1Length, bone2Length)
 	return {
 		bone1Length = bone1Length,
 		bone2Length = bone2Length,
@@ -13,32 +13,39 @@ local function createArm(root, bone1Length, bone2Length)
 end
 
 function love.load()
-	arm = createArm(vec2(100, 100), 100, 50)
+	leg = createLeg(vec2(100, 100), 100, 50)
 end
 
 function love.update()
 	mouse_pos = vec2(love.mouse.getX(), love.mouse.getY())
-	local diff = mouse_pos - arm.joint1
+	local diff = mouse_pos - leg.joint1
 	local dist = (diff):length()
 	local atan = math.atan2(diff.y, diff.x)
 
-	if arm.bone1Length + arm.bone2Length < dist then
-		arm.angle1 = atan
-		arm.angle2 = arm.angle1 + 0 -- check if thats right
+	if leg.bone1Length + leg.bone2Length < dist then
+		leg.angle1 = atan
+		leg.angle2 = leg.angle1 + 0 -- check if thats right
+	else 
+		cosAngle0 = ((dist * dist) + (leg.bone1Length * leg.bone1Length) - (leg.bone2Length * leg.bone2Length)) / (2 * dist * leg.bone1Length)
+        leg.angle1 = atan - math.acos(cosAngle0)
+
+        cosAngle1 = ((leg.bone2Length * leg.bone2Length) + (leg.bone1Length * leg.bone1Length) - (dist * dist)) / (2 * leg.bone2Length * leg.bone1Length)
+        leg.angle2 = leg.angle1 + math.pi - math.acos(cosAngle1)
+
 	end
 
-	arm.joint2 = arm.joint1 + vec2(math.cos(arm.angle1), math.sin(arm.angle1)) * arm.bone1Length
-	arm.hand = arm.joint2 + vec2(math.cos(arm.angle2), math.sin(arm.angle2)) * arm.bone2Length
+	leg.joint2 = leg.joint1 + vec2(math.cos(leg.angle1), math.sin(leg.angle1)) * leg.bone1Length
+	leg.hand = leg.joint2 + vec2(math.cos(leg.angle2), math.sin(leg.angle2)) * leg.bone2Length
 end
 
 function love.draw()
 	love.graphics.setColor(1.0, 1.0, 1.0)
-	love.graphics.circle("line", arm.joint1.x, arm.joint1.y, 10)
-	love.graphics.circle("line", arm.joint2.x, arm.joint2.y, 10)
-	love.graphics.line(arm.joint1.x, arm.joint1.y, arm.joint2.x, arm.joint2.y)
+	love.graphics.circle("line", leg.joint1.x, leg.joint1.y, 10)
+	love.graphics.circle("line", leg.joint2.x, leg.joint2.y, 10)
+	love.graphics.line(leg.joint1.x, leg.joint1.y, leg.joint2.x, leg.joint2.y)
 
-	love.graphics.circle("line", arm.hand.x, arm.hand.y, 10)
-	love.graphics.line(arm.joint2.x, arm.joint2.y, arm.hand.x, arm.hand.y)
+	love.graphics.circle("line", leg.hand.x, leg.hand.y, 10)
+	love.graphics.line(leg.joint2.x, leg.joint2.y, leg.hand.x, leg.hand.y)
 
 	love.graphics.setColor(1.0, 0, 0)
 	love.graphics.circle("line", mouse_pos.x, mouse_pos.y, 5)
