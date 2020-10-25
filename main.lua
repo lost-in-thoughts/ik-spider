1b9888cbd6768ab0e43404db2c2bbe47ed45dc7e
ichini <svenerdem@yahoo.de>
2020-10-25 17:38:27 +0100

    many points

--- main.lua
+++ main.lua
@@ -1,10 +1,17 @@
+require 'vec2'
+
 function love.load()
 end
 
 function love.update()
-	y = math.sin(love.timer.getTime()) * 100
+	joints = {}
+	for i=1, 10 do
+		table.insert(joints, vec2(i * 50))
+	end
 end
 
 function love.draw()
-	love.graphics.circle("fill", 200, 200 + y, 100)
+	for _, joint in ipairs(joints) do
+		love.graphics.circle("fill", joint.x, joint.y, 10)
+	end
 end
