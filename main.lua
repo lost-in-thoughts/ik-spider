942c1de491d041d6a9b6db2380d36cfdf3ffcb94
Philipp Bönsch <philipp.boensch@softgames.de>
2020-10-25 18:08:02 +0100

    arms and mouse rendered

--- main.lua
+++ main.lua
@@ -1,17 +1,27 @@
 require 'vec2'
 
+local function createArm()
+	return {
+		joint1 = vec2(100, 100),
+		joint2 = vec2(150, 150),
+		hand = vec2(150, 200)
+	}
+end
+
 function love.load()
+	arm = createArm()
 end
 
 function love.update()
-	joints = {}
-	for i=1, 10 do
-		table.insert(joints, vec2(i * 50))
-	end
+	mouse_pos = vec2(love.mouse.getX(), love.mouse.getY())
 end
 
 function love.draw()
-	for _, joint in ipairs(joints) do
-		love.graphics.circle("fill", joint.x, joint.y, 10)
+	love.graphics.setColor(1.0, 1.0, 1.0)
+	for i, node in pairs(arm) do
+		love.graphics.circle("line", node.x, node.y, 10)
 	end
+
+	love.graphics.setColor(1.0, 0, 0)
+	love.graphics.circle("line", mouse_pos.x, mouse_pos.y, 5)
 end
