b9e6d7e0115247ee41674b29548ac8d6e7f1d026
Philipp Bönsch <philipp.boensch@softgames.de>
2020-10-27 09:25:00 +0100

    commented code and references added

--- leg.lua
+++ leg.lua
@@ -1,3 +1,5 @@
+-- calculates a target position for the leg offsetted and
+-- slightly moved to the out, based on the spiders body position
 local function getTargetPos(bodyPos, legOffset)
 	return bodyPos + legOffset * 4 + vec2(0, 100)
 end
@@ -6,14 +8,17 @@ local function updateLeg(self, bodyPos)
 	local targetPos = getTargetPos(bodyPos, self.offset)
 	local targetDiff = (targetPos - self.targetPos):length()
 
+	-- to achieve a leg animation only update the leg position when
+	-- when a specified threshold/distance is reached (updateDist)
 	if targetDiff > self.updateDist then
 		self.currentTargetPos = self.targetPos
 		local future = targetPos - self.targetPos
-		self.targetPos = targetPos
-		self.targetPos = self.targetPos + (vec2(math.random(), math.random()) * 20 - vec2(10))
+		-- offset legs target position by random number to make it look more natural
+		self.targetPos = targetPos + (vec2(math.random(), math.random()) * 20 - vec2(10))
 		self.targetPos = self.targetPos + future * .5
 	end
 
+	-- animate the leg over time
 	local diff = self.targetPos - self.currentTargetPos
 	local speed = 15
 	if diff:length() < speed then
@@ -22,6 +27,8 @@ local function updateLeg(self, bodyPos)
 		self.currentTargetPos = self.currentTargetPos + diff:normalize() * speed
 	end
 
+	-- using this explanation to calculate the angles between body - bone1
+	-- and bone1 - bone2 https://www.alanzucconi.com/2018/05/02/ik-2d-1/
 	self.joint1 = bodyPos + self.offset
 	local diff = self.currentTargetPos - self.joint1
 	local dist = (diff):length()
@@ -29,7 +36,7 @@ local function updateLeg(self, bodyPos)
 
 	if self.bone1Length + self.bone2Length < dist then
 		self.angle1 = atan
-		self.angle2 = self.angle1 + 0 -- check if thats right
+		self.angle2 = self.angle1 + 0
 	else 
 		local cosAngle0 = ((dist * dist) + (self.bone1Length * self.bone1Length) - (self.bone2Length * self.bone2Length)) / (2 * dist * self.bone1Length)
 		self.angle1 = atan - math.acos(cosAngle0)
@@ -39,6 +46,8 @@ local function updateLeg(self, bodyPos)
 
 	end
 	
+	-- flipping the leg by mirroring the angles based on the angle of
+	--  joint1 - targetPos
 	if self.flipped then
 		local diff = self.angle1 - atan
 		self.angle1 = atan - diff
@@ -46,20 +55,30 @@ local function updateLeg(self, bodyPos)
 		self.angle2 = atan - diff
 	end
 
+	-- calculating the angles https://www.alanzucconi.com/2018/05/02/ik-2d-2/
 	self.joint2 = self.joint1 + vec2(math.cos(self.angle1), math.sin(self.angle1)) * self.bone1Length
-	self.hand = self.joint2 + vec2(math.cos(self.angle2), math.sin(self.angle2)) * self.bone2Length
+	self.foot = self.joint2 + vec2(math.cos(self.angle2), math.sin(self.angle2)) * self.bone2Length
 end
 
 local function drawLeg(self)
 	love.graphics.setColor(1.0, 1.0, 1.0)
-	-- love.graphics.circle("line", self.joint1.x, self.joint1.y, 10)
-	-- love.graphics.circle("line", self.joint2.x, self.joint2.y, 10)
 	love.graphics.line(self.joint1.x, self.joint1.y, self.joint2.x, self.joint2.y)
 
-	love.graphics.circle("fill", self.hand.x, self.hand.y, 5)
-	love.graphics.line(self.joint2.x, self.joint2.y, self.hand.x, self.hand.y)
+	love.graphics.circle("fill", self.foot.x, self.foot.y, 5)
+	love.graphics.line(self.joint2.x, self.joint2.y, self.foot.x, self.foot.y)
 end
 
+-- joint1: position of the first leg joint (positioned on the spiders body)
+-- joint2: position of the second leg joint (dist = bone1Length)
+-- foot: position of the legs foot (based on the target position)
+-- bone1Length: length of the bone between joint1 and joint2
+-- bone2Length: length of the bone between joint2 and foot
+-- angle1: angle between joint1 and joint2
+-- angle2: angle between joint2 and foot
+-- offset: the offsetted position the legs first joint is placed based on the spiders position
+-- targetPos: new target position for the leg which is calculated after a certain
+-- distance to the previous position has been reached
+-- currentTargetPos: for animating the leg movement
 function createLeg(offset, bone1Length, bone2Length, updateDist, flipped)
 	flipped = flipped or false
 
@@ -71,7 +90,7 @@ function createLeg(offset, bone1Length, bone2Length, updateDist, flipped)
 		offset = offset,
 		joint1 = offset,
 		joint2 = offset + vec2(bone1Length, 0),
-		hand = offset + vec2(bone1Length + bone2Length, 0),
+		foot = offset + vec2(bone1Length + bone2Length, 0),
 		flipped = flipped,
 		targetPos = getTargetPos(vec2(), offset),
 		currentTargetPos = vec2(),
--- spider.lua
+++ spider.lua
@@ -3,19 +3,22 @@ require 'leg'
 function createSpider(pos)
 	local spider = {
 		pos = pos,
+		speed = 200,
 		legs = {
+			-- creating 8 legs with different offsets, lengths and move thresholds
 			createLeg(vec2(-55, 0), 75, 200, 45, true),
 			createLeg(vec2(-50, 0), 75, 150, 50, true),
 			createLeg(vec2(-35, 0), 75, 150, 60, true),
-			createLeg(vec2(-20, 0), 75, 150, 80, true),
-			createLeg(vec2(20, 0), 75, 150, 85),
+			createLeg(vec2(-20, 0), 50, 110, 60, true),
+			createLeg(vec2(20, 0), 50, 110, 65),
 			createLeg(vec2(35, 0), 75, 150, 62),
 			createLeg(vec2(50, 0), 75, 150, 52),
 			createLeg(vec2(55, 0), 75, 200, 46),
 		},
 	}
 
-	function spider:update(dt, targetPos)
+	-- update the spiders position based on keyboard input
+	function spider:update(dt)
 		local dirx = love.keyboard.isDown("a") and -1 or
 			love.keyboard.isDown("d") and 1 or
 			0
@@ -26,19 +29,22 @@ function createSpider(pos)
 
 		local dir = vec2(dirx, diry)
 
-		self.pos = self.pos + dir * dt * 200
+		self.pos = self.pos + dir * dt * self.speed
 
 		for _, leg in ipairs(self.legs) do
 			leg:update(self.pos)
 		end
 	end
 
+	-- draw the spiders body and legs
 	function spider:draw()
 		love.graphics.setColor(1.0, 1.0, 1.0)
 		love.graphics.ellipse("fill", self.pos.x, self.pos.y, 80, 50)
+		
 		for _, leg in ipairs(self.legs) do
 			leg:draw()
 		end
+		
 		love.graphics.setColor(0, 0, 0)
 		love.graphics.circle("fill", self.pos.x + 15, self.pos.y + 10, 10)
 		love.graphics.circle("fill", self.pos.x - 15, self.pos.y + 10, 10)
